ae7301f920c31ab16736555bec7f8cd1cae41767
Tanvi Garg <tanvi.garg23@gmail.com>
2018-11-30 10:52:09 -0800

    WordsController Result route to provide parsed results Correct Action Type test pass, changed return type from View  to RedirectToAction

--- WordCounter.Tests/ControllerTests/WordsControllerTests.cs
+++ WordCounter.Tests/ControllerTests/WordsControllerTests.cs
@@ -25,21 +25,6 @@ namespace WordCounter.Tests
     }
 
     [TestMethod]
-    public void Result_ReturnsCorrectView_True()
-    {
-      //Arrange
-      WordsController controller = new WordsController();
-
-      //Act
-      string inputLetterOrWord = "cAt";
-      string inputStringToCheck = "there is a bLack caT and a white CAt in the cathedral!!";
-      ActionResult resultView = controller.Result(inputLetterOrWord, inputStringToCheck);
-
-      //Assert
-      Assert.IsInstanceOfType(resultView,typeof(ViewResult));
-    }
-
-    [TestMethod]
     public void Result_ReturnsCorrectActionType_True()
     {
       //Arrange
